573dedb7cef4c22031c4f16612d58c7310cafd30
ionilancer <ionilancer@gmail.com>
2022-11-24 20:06:40 +0100

    chore: create menu

---
+++ client/src/pages/Profilee.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { MenuContainer } from "../components";
+import "./Home.css";
+
+const Profile = () => {
+  return (
+    <>
+      <MenuContainer />
+    </>
+  );
+};
+
+export default Profile;
